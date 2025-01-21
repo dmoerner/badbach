a3aed7d8a0d79778d859f592a2015f92e44ad99f
Daniel Moerner <dmoerner@gmail.com>
2025-01-21 16:13:01 -0500

    Partial implementation of Rules 4 & 5: Restrictions on start
    This implementation of Rule 4 is not quite right, but exploits the fact
    that my sample melodies start on the first note of the mode.

--- badbach.py
+++ badbach.py
@@ -25,7 +25,8 @@ allowed_intervals = [
 weight_delta = 10
 
 
-def next_note(cpprev, mprev, mcur):
+def next_note(cp, mprev, mcur):
+
     candidates = [
         Note("A"),
         Note("B"),
@@ -36,6 +37,15 @@ def next_note(cpprev, mprev, mcur):
         Note("G"),
     ]
     weights = [100 // len(candidates)] * len(candidates)
+
+    # Rule 4, Rule 5: Start must be different from melody start.
+    # This is not quite correct, since we really should be checking for
+    # 1, 3, and 5 within the key.
+    if len(cp) == 0:
+        cp.add_notes(random.choice([c for c in candidates if c != mcur]))
+        return
+
+    cpprev = Note(cp[-1][-1][2][0])
 
     for i in range(len(candidates)):
         # Rule 1: No harmonic invervals are consonances.
@@ -54,34 +64,27 @@ def next_note(cpprev, mprev, mcur):
             if Note(fifth) in candidates:
                 weights[candidates.index(Note(fifth))] += weight_delta
 
-    cpcur = random.choices(list(candidates), weights=weights)
-
-    return cpcur
+    cp.add_notes(random.choices(list(candidates), weights=weights))
+    return
 
 
 def create_counterpoint(melody):
     # A melody is made up of bars, and each note object is a list whose third element is a list of names.
     cp = Track()
     prev = melody[0][0][2][0]
-    cp.add_notes(prev)
     for bar_i in range(len(melody)):
         for note_i in range(len(melody[bar_i])):
-            if bar_i == 0 and note_i == 0:
-                continue
-            cp.add_notes(
-                next_note(
-                    Note(cp[-1][-1][2][0]),
-                    Note(prev),
-                    Note(melody[bar_i][note_i][2][0]),
-                )
+            next_note(
+                cp,
+                Note(prev),
+                Note(melody[bar_i][note_i][2][0]),
             )
             prev = melody[bar_i][note_i][2][0]
 
     return cp
 
 
-def create_melody():
-    melody = ["C", "D", "E", "F", "G", "F", "E", "D", "C"]
+def create_melody(melody=["C", "D", "E", "F", "G", "F", "E", "D", "C"]):
     t = Track()
     for n in melody:
         t.add_notes(n)
@@ -96,7 +99,24 @@ def tracks_to_composition(lst):
 
 
 def main():
-    melody = create_melody()
+    twinkle = [
+        "C",
+        "C",
+        "G",
+        "G",
+        "A",
+        "A",
+        "G",
+        "G",
+        "F",
+        "F",
+        "E",
+        "E",
+        "D",
+        "D",
+        "C",
+    ]
+    melody = create_melody(twinkle)
     cp = create_counterpoint(melody)
     c = tracks_to_composition([melody, cp])
     print(c)
